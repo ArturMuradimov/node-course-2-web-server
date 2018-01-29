b29676eeb2b2ee616651cb116bda78c4530a7538
Artur Muradimov <artur.muradimov@aplana.com>
2018-01-29 20:09:15 +0500

    Add project page

--- server.js
+++ server.js
@@ -60,8 +60,13 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page'
   });
 });
 
